beaeaf3586dfff3d19a220ab23cc23336c2712ec
BugHunter-X <askzenai@gmail.com>
2025-01-30 07:51:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+<div class="flex">
+  <div class="bg-red-500 p-4 w-1/2"></div>
+  <div class="bg-blue-500 p-4 w-1/2"></div>
+</div>
+```
+This code snippet uses Tailwind CSS classes to create a flex container with two equally sized div elements. However, if you inspect the rendered output you may find that the divs don't perfectly take up half the width, especially when there's additional content or padding.
